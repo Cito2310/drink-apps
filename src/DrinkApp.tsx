b420bcf73f60212ec1c542c343e5587d4840059e
Cito2310 <cito2310@gmail.com>
2022-11-28 05:27:07 -0300

    Init project react-ts-drink-app-2

---
+++ src/DrinkApp.tsx
@@ -0,0 +1,5 @@
+export const DrinkApp = () => {
+    return (
+        <div>DrinkApp</div>
+    )
+}
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { DrinkApp } from './DrinkApp'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  // <React.StrictMode>
+    <DrinkApp/>
+  // </React.StrictMode>
+)
