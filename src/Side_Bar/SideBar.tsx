42128dc26ff50926783a33694713fccc31a2774e
Cito2310 <cito2310@gmail.com>
2022-11-29 20:42:20 -0300

    Added styled to btn-navlink-sidebar

--- src/Side_Bar/SideBar.tsx
+++ src/Side_Bar/SideBar.tsx
@@ -1,24 +1,27 @@
 import { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { contextSideBar } from './ProviderSideBarController';
 
 import "./side-bar.scss"
+import "./btn-nav-link.scss"
 
 export const SideBar = () => {
     const { sidebarOpen, toggleSidebar } = useContext(contextSideBar)
+    console.log(window.location.pathname === "/select")
+    console.log(window.location.pathname === "/")
 
     return (
         <div className={sidebarOpen ? "sidebar open left" : "sidebar left"}> {/* PARA CAMBIAR EL LADO DONDE SE DESPLEGARA LA BARRA LATERAL CAMBIAR right | left */}
             {/* MODIFICAR DESDE AQUI */}
             <div className='sidebar-section-top'>
-                <h2>Titulo</h2>
+                <h2>Drink App</h2>
                 <i onClick={toggleSidebar} className="fa-solid fa-xmark"></i> {/* ESTE ELEMENTO ES REQUERIDO - NO BORRAR */}
             </div>
 
             <div className='sidebar-section-container'>
-                <h3>Texto Uno</h3>
-                <h3>Texto Dos</h3>
-                <h3>Texto Tres</h3>
+                <NavLink className={window.location.pathname === "/" ? "btn-navlink-sidebar selected" : "btn-navlink-sidebar" } to="/">Todos los productos</NavLink>
+                <NavLink className={window.location.pathname === "/select" ? "btn-navlink-sidebar selected" : "btn-navlink-sidebar" } to="/select">Solo productos seleccionados</NavLink>
             </div>
             {/* MODIFICAR HASTA AQUI */}
         </div>
