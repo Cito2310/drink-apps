dc864054fb7b416378a32d9c4772328c3e2c70f9
Cito2310 <cito2310@gmail.com>
2023-05-13 16:02:37 -0300

    added tailwindcss

--- src/components/BlackScreen.tsx
+++ src/components/BlackScreen.tsx
@@ -5,12 +5,8 @@ import "../styles/black-screen.scss"
 export const BlackScreen = () => {
     useEffect(() => {
         document.body.style.overflow = "hidden"
-    
-        return () => {
-            document.body.style.overflow = "visible"
-        }
+        return (() => {document.body.style.overflow = "visible"})
     }, [])
-    
 
-    return <div id="black-screen"/>
+    return ( <div id="black-screen"/> )
 }
--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,15 +1,10 @@
 import "../styles/loading-screen.scss"
 
-export const LoadingScreen = () => {
-    return (
-        <div id="loading-screen">
-            <div id="container-loading">
-                <svg className="spinner" viewBox="0 0 50 50">
-                    <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
-                </svg>
-
-                <p className="text-loading">Cargando<br/>Disculpe la espera</p>
-            </div>
+export const LoadingScreen = () => (
+    <div id="loading-screen">
+        <div id="container-loading">
+            <svg className="spinner" viewBox="0 0 50 50"><circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle></svg>
+            <p className="text-loading">Cargando<br/>Disculpe la espera</p>
         </div>
-    )
-}
+    </div>
+)
